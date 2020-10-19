f065c0621552636b27ccd87684e502781f42552a
DavidStahl97 <david.stahl97@outlook.com>
2020-10-19 13:34:16 +0200

    reformat

--- Einstieg/mvgAvg.py
+++ Einstieg/mvgAvg.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from matplotlib import pyplot
+
 # read csv into a dataframe
-ts=pd.read_csv("airline-passengers.csv",header=0,index_col=0)
+ts = pd.read_csv("airline-passengers.csv", header=0, index_col=0)
 rolling = ts.rolling(window=3)
 rolling_mean = rolling.mean()
 rolling_mean.plot()
