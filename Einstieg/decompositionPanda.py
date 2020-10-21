5a5ea48e95a79a9bcf86bbf703848f501e9bf1e2
DavidStahl97 <david.stahl97@outlook.com>
2020-10-21 12:43:19 +0200

    rename

--- Einstieg/decompositionPanda.py
+++ Einstieg/decompositionPanda.py
@@ -3,8 +3,10 @@ from matplotlib import pyplot
 
 # read csv into a dataframe
 ts = pd.read_csv("airline-passengers.csv", header=0, index_col=0)
-rolling = ts.rolling(window=3)
+rolling = ts.rolling(window=10)
 rolling_mean = rolling.mean()
 rolling_mean.plot()
+
+seasonal = ts["Passengers"] - rolling_mean
 
 pyplot.show()
